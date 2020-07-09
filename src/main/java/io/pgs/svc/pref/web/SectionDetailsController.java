4722abed3d733d7315ccb0a97e716480683f3924
dev-ljm <58322582+dev-ljm@users.noreply.github.com>
2020-07-09 14:23:41 +0900

    주차구획 상세 2:23

---
+++ src/main/java/io/pgs/svc/pref/web/SectionDetailsController.java
@@ -0,0 +1,11 @@
+package io.pgs.svc.pref.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/pref/section/details")
+public class SectionDetailsController {
+}
