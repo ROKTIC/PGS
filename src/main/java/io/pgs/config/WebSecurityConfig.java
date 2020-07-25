834f57eabaa88d8154d83dd3ff9628f9ebaaf2e9
dev-ljm <58322582+dev-ljm@users.noreply.github.com>
2020-07-25 15:03:01 +0900

    암호화 제거 3:02

--- src/main/java/io/pgs/config/WebSecurityConfig.java
+++ src/main/java/io/pgs/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*
+
         http.authorizeRequests()
                     .antMatchers("/**").hasRole("ADMIN")
                     .antMatchers("/login").permitAll()
@@ -34,7 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .formLogin().loginPage("/login").permitAll()
                 .and()
                     .csrf().disable();
-        */
+
+/*
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
@@ -44,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .defaultSuccessUrl("/", true)
                 .and()
                     .csrf().disable();
-
+        */
 
     }
 
@@ -61,6 +62,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new NoEncodingPasswordEncoder();
     }
 }
