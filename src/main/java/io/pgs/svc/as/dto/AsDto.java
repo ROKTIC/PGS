a4e088e67f106fc6553d6ec7e230c7e171cd4f8f
YUNINROK <inroktic@naver.com>
2021-04-14 14:58:56 +0900

    Update AsDto.java

--- src/main/java/io/pgs/svc/as/dto/AsDto.java
+++ src/main/java/io/pgs/svc/as/dto/AsDto.java
@@ -1,13 +1,19 @@
 package io.pgs.svc.as.dto;
 
+import io.pgs.cmn.PageDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.sql.Timestamp;
+
 @Setter
 @Getter
 @ToString
-public class AsDto {
+public class AsDto extends PageDto {
+
+    private Timestamp createdAt;
+    private Integer as_type;
 
 
 
