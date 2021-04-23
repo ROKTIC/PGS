6a50b7b43a2499cad5497070d73da60b988b05fc
YUNINROK <inroktic@naver.com>
2021-04-23 15:26:09 +0900

    서비스 구현

--- src/main/java/io/pgs/svc/as/service/impl/AsServiceImpl.java
+++ src/main/java/io/pgs/svc/as/service/impl/AsServiceImpl.java
@@ -1,6 +1,15 @@
 package io.pgs.svc.as.service.impl;
 
 import io.pgs.svc.as.service.AsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(transactionManager = "mariaTransactionManager")
 public class AsServiceImpl implements AsService {
 }
--- src/main/java/io/pgs/svc/as/service/impl/RegistServiceImpl.java
+++ src/main/java/io/pgs/svc/as/service/impl/RegistServiceImpl.java
@@ -1,6 +1,16 @@
 package io.pgs.svc.as.service.impl;
 
 import io.pgs.svc.as.service.RegistService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional(transactionManager = "mariaTransactionManager")
 
 public class RegistServiceImpl implements RegistService {
 
