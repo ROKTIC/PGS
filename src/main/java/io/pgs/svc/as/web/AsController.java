5d1e4f8686d1ad5af87365acb75bead41229eff2
YUNINROK <inroktic@naver.com>
2021-04-26 14:34:12 +0900

    commit

--- src/main/java/io/pgs/svc/as/web/AsController.java
+++ src/main/java/io/pgs/svc/as/web/AsController.java
@@ -3,6 +3,8 @@ package io.pgs.svc.as.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class AsController {
 
